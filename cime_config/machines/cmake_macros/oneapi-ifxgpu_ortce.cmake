c61c0e4adb49f831a0982022e1c72351ab5ac10b
Omar Khalil Ahmed <omar.ahmed@intel.com>
2023-11-06 16:08:08 -0800

    Move to AOT

--- cime_config/machines/cmake_macros/oneapi-ifxgpu_ortce.cmake
+++ cime_config/machines/cmake_macros/oneapi-ifxgpu_ortce.cmake
@@ -4,7 +4,9 @@ string(APPEND SLIBS " -lmkl_intel_lp64 -lmkl_sequential -lmkl_core")
 string(APPEND SLIBS " -fiopenmp -fopenmp-targets=spir64")
 set(USE_SYCL "TRUE")
 string(APPEND KOKKOS_OPTIONS " -DCMAKE_CXX_STANDARD=17 -DKokkos_ENABLE_OPENMP=ON -DKokkos_ARCH_INTEL_PVC=ON -DKokkos_ENABLE_SYCL=ON -DKokkos_ENABLE_EXPLICIT_INSTANTIATION=Off")
-string(APPEND SYCL_FLAGS " -\-intel -fsycl -fsycl-targets=spir64_gen -mlong-double-64 -Xsycl-target-backend \"-device pvc\"")
-#string(APPEND SYCL_FLAGS " -\-intel -fsycl")
-string(APPEND CXX_LDFLAGS " -Wl,-\-defsym,main=MAIN_\_ -lifcore -\-intel -fsycl -lsycl -mlong-double-64 -Xsycl-target-backend \"-device pvc\"")
+#string(APPEND SYCL_FLAGS " -\-intel -fsycl -fsycl-targets=spir64_gen -mlong-double-64 -Xsycl-target-backend \"-device pvc\"")
+string(APPEND SYCL_FLAGS " -\-intel -fsycl -mlong-double-64")
+#string(APPEND CXX_LDFLAGS " -Wl,-\-defsym,main=MAIN_\_ -lifcore -\-intel -fsycl -lsycl -mlong-double-64 -Xsycl-target-backend \"-device pvc\"")
+string(APPEND CXX_LDFLAGS " -Wl,-\-defsym,main=MAIN_\_ -lifcore -\-intel -fsycl -lsycl -mlong-double-64")
+
 
